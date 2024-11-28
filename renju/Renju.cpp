42c3dd43de6720c95585e95bf5d44d16147f5eed
tangent95 <tangent399@gmail.com>
2024-11-28 21:57:37 +0800

    feat: 添加五子棋库访问的接口

---
+++ renju/Renju.cpp
@@ -0,0 +1,9 @@
+﻿#include "Renju.hpp"
+
+namespace renju
+{
+	const char* GetAuthorCString()
+	{
+		return "差点忘改这里的内容了，原本这里是真名";
+	}
+}
---
+++ renju/Renju.hpp
@@ -0,0 +1,15 @@
+﻿#ifndef HEADER_RENJU
+#define HEADER_RENJU
+
+#include "RenjuDefination.hpp"
+#include "Board.hpp"
+#include "StoneRegion.hpp"
+#include "SuccessionJudge.hpp"
+
+namespace renju
+{
+	const char* GetAuthorCString();
+}
+
+
+#endif
