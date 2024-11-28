28888f891684d39e247b98a6b5d9b9183204e4a2
tangent95 <tangent399@gmail.com>
2024-11-28 22:16:11 +0800

    feat: 添加gui主界面

---
+++ qtrenju/PlayWindow.cpp
@@ -0,0 +1,111 @@
+﻿#include "PlayWindow.hpp"
+#include <QtWidgets\QMessageBox>
+#include <QtGui\QPainter>
+
+#include "MainBoard.hpp"
+#include "MCTS\RolloutMCTS.hpp"
+
+//#ifdef MSVC
+#pragma execution_character_set("utf-8")
+//#endif
+
+PlayWindow::PlayWindow(QWidget* parent, Qt::WindowFlags flags) : QWidget(parent, flags)
+{
+	InitializeUI();
+	InitializeConnection();
+}
+PlayWindow::~PlayWindow()
+{
+	
+}
+void SetStatusText(QLabel* label)
+{
+	QString text;
+	text += "第";
+	if (mainBoard.IsBoardEmpty())
+	{
+		text += QString::number(0 + 1);
+	}
+	else
+	{
+		text += QString::number(mainBoard.GetCurrentOrder() + 1);
+	}
+	text += "步\n";
+	
+	text += "轮走方:";
+	if (mainBoard.IsBoardEmpty())
+	{
+		text += "黑";
+	}
+	else
+	{
+		if (mainBoard.GetCurrentType() == renju::StoneType::black)
+		{
+			text += "白";
+		}
+		else
+		{
+			text += "黑";
+		}
+	}
+	label->setText(text);
+}
+void PlayWindow::NotifyThis()
+{
+	SetStatusText(statusLabel);
+}
+void PlayWindow::InitializeUI()
+{
+	mainLayout = new QHBoxLayout;
+	controlLayout = new QVBoxLayout;
+	boardWindow = new BoardWindow(nullptr, Qt::WindowFlags(), this);
+	boardWindow->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+	statusLabel = new QLabel;
+	SetStatusText(statusLabel);
+	statusLabel->setSizePolicy(statusLabel->sizePolicy().horizontalPolicy(), QSizePolicy::Minimum);
+	statusLabel->setStyleSheet("border: 1px solid black;");
+	restartButton = new QPushButton("重新开始");
+	retractButton = new QPushButton("悔棋");
+	scorePlaceButton = new QPushButton("打分落子");
+	MCTSPlaceButton = new QPushButton("MCTS落子");
+	
+	controlLayout->setSpacing(2);
+	controlLayout->addWidget(statusLabel, 0, Qt::AlignTop);
+	controlLayout->addWidget(restartButton, 0, Qt::AlignTop);
+	controlLayout->addWidget(retractButton, 0, Qt::AlignTop);
+	controlLayout->addWidget(scorePlaceButton, 0, Qt::AlignTop);
+	controlLayout->addWidget(MCTSPlaceButton, 0, Qt::AlignTop);
+	
+	mainLayout->addWidget(boardWindow);
+	mainLayout->addLayout(controlLayout);
+	this->setLayout(mainLayout);
+}
+#include <cstdlib>
+#include <ctime>
+void PlayWindow::InitializeConnection()
+{
+	connect(restartButton, &QPushButton::clicked, [&]() { mainBoard.Restart(); boardWindow->update(); NotifyThis(); });
+	connect(retractButton, &QPushButton::clicked, [&]() { mainBoard.RetractMove(); boardWindow->update(); NotifyThis(); });
+	connect(MCTSPlaceButton, &QPushButton::clicked, [&]() { 
+		renju::RolloutMCTS mcts;
+		mcts.SetLimitation(renju::LimitationType::iteration, 0, 200000, 100);
+		mainBoard.DoMove(mcts.GetMove(mainBoard, 2));
+		boardWindow->update();
+		NotifyThis();
+		});
+	connect(scorePlaceButton, &QPushButton::clicked, [&]() { 
+		renju::RolloutMCTS mcts;
+		mcts.ScorePlace(mainBoard, rand());
+		boardWindow->update();
+		NotifyThis();
+		});
+	srand((renju::UINT_32)time(NULL));
+}
+void PlayWindow::resizeEvent(QResizeEvent* event)
+{
+	
+}
+void PlayWindow::mousePressEvent(QMouseEvent *event)
+{
+	
+}
---
+++ qtrenju/PlayWindow.hpp
@@ -0,0 +1,37 @@
+﻿#ifndef HEADER_QT_PLAYWINDOW
+#define HEADER_QT_PLAYWINDOW
+#include <QtCore\QObject>
+#include <QtWidgets\QWidget>
+#include <QtWidgets\QPushButton>
+#include <QtWidgets\QLabel>
+#include <QtGui\QResizeEvent>
+#include <QtGui\QMouseEvent>
+#include <QtWidgets\QHBoxLayout>
+#include <QtWidgets\QVBoxLayout>
+
+#include "BoardWindow.hpp"
+#include "Notify.hpp"
+
+class PlayWindow : public QWidget, public Notify
+{
+public:
+	PlayWindow(QWidget* parent = nullptr, Qt::WindowFlags flags = Qt::WindowFlags());
+	~PlayWindow();
+	void NotifyThis() override;
+protected:
+	void InitializeUI();
+	void InitializeConnection();
+	void resizeEvent(QResizeEvent* event) override;
+	void mousePressEvent(QMouseEvent *event) override;
+public:
+	QPushButton* restartButton;
+	QPushButton* retractButton;
+	QPushButton* scorePlaceButton;
+	QPushButton* MCTSPlaceButton;
+	QLabel* statusLabel;
+	
+	QHBoxLayout* mainLayout;
+	QVBoxLayout* controlLayout;
+	BoardWindow* boardWindow;
+};
+#endif
